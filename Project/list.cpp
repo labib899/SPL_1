3cd86d2219a86a2c2b97ed4fdebb7674c9740042
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2023-02-06 06:42:21 +0600

    Update and rename devicesList.cpp to list.cpp

--- Project/list.cpp
+++ Project/list.cpp
@@ -2,28 +2,31 @@
 #include <pcap.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-//#include <sys/socket.h>
+
 
 using namespace std;
 
-//function to display the list of available devices on the network
+
+// function to display the list of network interfaces
 void list();
 void dump_addresses(pcap_addr_t *addresses);
+
 
 
 int main()
 {
     list();
-	return 0;
+    return 0;
 }
+
 
 
 void list()
 {
     pcap_if_t *device;
     pcap_if_t *interface;
-    
     char err[PCAP_ERRBUF_SIZE];
+    
     if(pcap_findalldevs(&interface,err)==-1)
     {
         cout<<err<<endl;
@@ -31,17 +34,18 @@ void list()
     }
     else
     {
-        cout<<"List of devices:"<<endl;
+        cout<<"List of network interfaces:"<<endl<<endl;
         int i=1;
         for(device=interface;device!=NULL;device=device->next,i++)
         {
-            printf("Device %d: %s:  ",i,device->name);
+            printf("%d: %s: \n",i,device->name);
             dump_addresses(device->addresses);
             printf("\n");
         }
         return;
     }
 }
+
 
 void dump_addresses(pcap_addr_t *addresses)
 {
@@ -50,10 +54,14 @@ void dump_addresses(pcap_addr_t *addresses)
     {
         struct sockaddr_in *ip=(struct sockaddr_in *)addr->addr;
         struct sockaddr_in *nm=(struct sockaddr_in *)addr->netmask;
-        if(ip && nm) cout<<inet_ntoa(ip->sin_addr)<<" / "<<inet_ntoa(nm->sin_addr);
+        if(ip && nm)
+        {
+            cout<<"\tIP Address: "<<inet_ntoa(ip->sin_addr)<<endl;
+            cout<<"\tSubnet Mask: "<<inet_ntoa(nm->sin_addr)<<endl;
+        }
         addr=addr->next;
     }
 }
 
 
-// g++ devicesList.cpp -lpcap
+// g++ list.cpp -lpcap
