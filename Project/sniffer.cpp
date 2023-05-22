c86afd904540f4a65ce4519f66c2e6d49ace4722
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2023-05-22 10:11:09 +0600

    Update sniffer.cpp

--- Project/sniffer.cpp
+++ Project/sniffer.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <pcap.h>
+#include <math.h>
 #include <string>
+#include <sstream>
 #include <map>
-#include "tcp_header.h"
-#include "udp_header.h"
-#include "icmp_header.h"
-#include "ip_header.h"
+#include <iterator>
+#include "protocols/eth_header.h"
+#include "protocols/tcp_header.h"
+#include "protocols/udp_header.h"
+#include "protocols/icmp_header.h"
+#include "protocols/ip_header.h"
+#include "protocols/http_parser.h"
+
 
 
 using namespace std;
@@ -17,7 +23,12 @@ using namespace std;
 
 
 
-void callback(u_char *user,const struct pcap_pkthdr *header,const u_char *packet);
+// to keep track lost packets
+map<uint,int> ack_map;
+
+
+
+void got_packet(u_char *user,const struct pcap_pkthdr *header,const u_char *packet);
 
 
 
@@ -52,19 +63,25 @@ int main()
         return -1;
     }
     
-    /*cout<<"Enter the number of packets to be captured: ";
+    /*
+    cout<<"Enter the number of packets to be captured: ";
     int numOfPackets;
     cin>>numOfPackets;
-    cout<<endl;*/
+    cout<<endl;
+    */
     // starting the packet capture loop 
     // -1 instead of numOfPackets to capture indefinitely
-    pcap_loop(handle,-1,callback,NULL);
+    pcap_loop(handle,-1,got_packet,NULL);
+
     
     // closing the device
     pcap_close(handle);
+
     
     // freeing the device list
     pcap_freealldevs(devices);
+
+
     
     return 0;
 }
@@ -75,86 +92,127 @@ int main()
 user = user defined data structure (not used), pcap_pkthdr = info about the packet,
 packet = pointer to the packet data
 */
-void callback(u_char *user, const struct pcap_pkthdr *header, const u_char *packet) 
+void got_packet(u_char *user, const struct pcap_pkthdr *header, const u_char *packet) 
 {   
-    cout<<"Packet Length: "<<header->len<<endl;
-
-    // display the time stamp of the packet
-    cout << "Time stamp: " << header->ts.tv_sec << "." << header->ts.tv_usec << endl;
-
-    map<string,int> protocols;
+    struct eth_header* eth=(struct eth_header*) packet;
+    //packet+=sizeof(eth_header); // jump over ethernet header (14 bytes)
 
 
-    // for counting dropped packets
-    int drop_count = 0;
-    
-    if (header->len != header->caplen)
+    // 0x0800 is IP type
+    if(ntohs(eth->ether_type)==0x0800)    
     {
-        drop_count++;
-    }
-    // analyzing the packet
-    for(int i=0;i<header->len;i++)
-    {
-        // extracting the protocol from the packet
-        string protocol = "Unknown";
-        if(packet[i]==6)
+        struct ip_header *ip=(struct ip_header*) (packet+sizeof(eth_header));
+        printIPHeader(ip);
+        //packet+=sizeof(ip_header); // jump over ip header
+
+        int ip_header_size = (int)ip->header_len * 4;
+        if(ip_header_size<20 || ip_header_size>60)
         {
-            protocol="TCP";
-            // extracting the TCP header from the packet
-            const struct tcp_header* tcp = (const struct tcp_header*)(packet + i + sizeof(uint8_t));
-            // printing the TCP header fields
-            cout << "TCP header:" << endl;
-            cout << "  Source port: " << tcp->source_port << endl;
-            cout << "  Destination port: " << tcp->dest_port << endl;  
-            cout << "  Sequence number: " << tcp->sequence_num << endl;
-            cout << "  Acknowledgement number: " << tcp->ack_num << endl;
-            cout << "  Header length: " << tcp->header_len << endl;
-            cout << "  Urgent pointer flag: " << tcp->urg_flag << endl;
-            cout << "  Acknowledgement flag: " << tcp->ack_flag << endl;
-            cout << "  Push flag: " << tcp->psh_flag << endl;
-            cout << "  Reset flag: " << tcp->rst_flag << endl;
-            cout << "  Synchronize flag: " << tcp->syn_flag << endl;
-            cout << "  Finish flag: " << tcp->fin_flag << endl;
-            cout << "  Window size: " << tcp->window_size << endl;
-            cout << "  Checksum: " << tcp->checksum << endl;
-            cout << "  Urgent pointer: " << tcp->urgent_ptr << endl<<endl;
+            cout << "Invalid IP Header" << endl;
+            return;
+        }
 
 
-            // printing the payload
-            const u_char *payload = packet + i + sizeof(uint8_t) + tcp->header_len*4;
-            // point to the location in memory where payload data starts
-            int payload_len = header->len - (i + sizeof(uint8_t) + tcp->header_len*4);
-            // payload len= header len - size of the header
-            if (payload_len > 0) 
+
+        // determining protocols
+        if(ip->protocol==6) // IPPROTO_TCP
+        {   
+            // extracting TCP header from the packet
+            struct tcp_header* tcp = (struct tcp_header*) (packet+sizeof(eth_header)+sizeof(ip_header));
+            //packet+=sizeof(tcp_header); // jump over tcp header
+
+
+            if(sizeof(tcp_header)<20 || sizeof(tcp_header)>60)
             {
-                cout << "Payload:" << endl;
-                for (int j = 0; j < payload_len; j++) 
+                cout << "Invalid TCP Header" << endl;
+                return;
+            }
+
+
+            // determine if the packet is part of an HTTP connection
+            bool isHTTP = (tcp->source_port == htons(80) || tcp->dest_port == htons(80));
+
+            if (isHTTP) 
+            {   
+                // Extract the HTTP payload
+                const u_char* httpPayload = packet+sizeof(eth_header)+sizeof(ip_header)+sizeof(tcp_header);
+                int httpPayloadLen = header->caplen - (sizeof(eth_header)+sizeof(ip_header)+sizeof(tcp_header));
+
+                // Parse the HTTP request or response
+                if (tcp->dest_port == htons(80)) 
                 {
-                    printf("%02x ", payload[j]);
-                    if ((j + 1) % 16 == 0) 
-                    {
-                        cout << endl;
-                    }
+                    HttpRequest request = parseHttpRequest(string(httpPayload,httpPayload + httpPayloadLen));
+                    printHttpRequest(request);
+                } 
+                else 
+                {
+                    HttpResponse response = parseHttpResponse(string(httpPayload,httpPayload + httpPayloadLen));
+                    printHttpResponse(response);
                 }
-                cout << endl<<endl<<endl;
+                
+                // display the HTTP payload
+                /*
+                cout << "HTTP Payload:" << endl;
+                for (int i = 0; i < httpPayloadLen; i++) 
+                {
+                    printf("%c", httpPayload[i]);
+                }
+                cout << endl << endl;
+                */
+            }
+
+            else
+            {   
+                // printing TCP header fields
+                printTCPHeader(tcp);
+
+                // Printing TCP payload
+                const u_char* payload = packet;
+                int payload_len = ntohs(ip->total_length) - sizeof(ip_header) - sizeof(tcp_header);
+                
+                if (payload_len > 0) 
+                {
+                    cout << "Payload:" << endl;
+                    for (int j = 0; j < payload_len; j++) 
+                    {
+                        printf("%02x ", payload[j]);
+                        if ((j + 1) % 16 == 0) 
+                        {
+                            cout << endl;
+                        }
+                    }
+                    cout << endl << endl << endl;
+                }
+
+                // updating ack_map
+                uint ACK=ntohl(tcp->ack_num);
+
+                // checking triple duplicate ACKs
+                if(ack_map[ACK]>3)
+                {
+                    cout<<"Triple duplicate ACKs detected. Packet loss may have occurred"<<endl<<endl<<endl;
+                    ack_map[ACK]=0; // resetting
+                }
             }
         }
-        else if(packet[i]==17)
-        {
-            protocol="UDP";
+
+
+        else if(ip->protocol==17) // IPPROTO_UDP
+        {   
             // extracting the UDP header from the packet
-            const struct udp_header* udp = (const struct udp_header*)(packet + i + sizeof(uint8_t));
+            struct udp_header* udp = (struct udp_header*) (packet+sizeof(eth_header)+sizeof(ip_header));
+            //packet+=sizeof(udp_header); // jump over udp header
+
+
             // printing the UDP header fields
-            cout << "UDP header:" << endl;
-            cout << "  Source port: " << udp->source_port << endl;
-            cout << "  Destination port: " << udp->dest_port << endl;
-            cout << "  Length: " << udp->length << endl;
-            cout << "  Checksum: " << udp->checksum << endl<<endl;
+            printUDPHeader(udp);
 
 
-            // printing the payload
-            const u_char *payload = packet + i + sizeof(uint8_t) + sizeof(struct udp_header);
-            int payload_len = header->len - (i + sizeof(uint8_t) + sizeof(struct udp_header));
+            // Printing UDP payload
+            const u_char* payload = packet+sizeof(eth_header)+sizeof(ip_header)+sizeof(udp_header);
+            int payload_len = ntohs(udp->length) - sizeof(udp_header); 
+
+
             if (payload_len > 0) 
             {
                 cout << "Payload:" << endl;
@@ -170,23 +228,23 @@ void callback(u_char *user, const struct pcap_pkthdr *header, const u_char *pack
             }
         }
 
-        else if(packet[i]==1) 
-        {
-            protocol="ICMP";
+
+
+        else if(ip->protocol==1) // IPPROTO_ICMP
+        {   
             // extracting the ICMP header from the packet
-            const struct icmp_header* icmp = (const struct icmp_header*)(packet + i + sizeof(uint8_t));
+            struct icmp_header* icmp = (struct icmp_header*) (packet+sizeof(eth_header)+sizeof(ip_header));
+            //packet+=sizeof(icmp_header);
+
+
             //printing the ICMP header fields
-            cout << "ICMP Header:" << endl;
-            cout << " Type: " << (int)icmp->type << endl;
-            cout << " Code: " << (int)icmp->code << endl;
-            cout << " Checksum: " << ntohs(icmp->checksum) << endl;
-            cout << " Identifier: " << ntohs(icmp->un.echo.id) << endl;
-            cout << " Sequence Number: " << ntohs(icmp->un.echo.seq) << endl<<endl;
+            printICMPHeader(icmp);
 
 
-            // printing the payload
-            const u_char *payload = packet + i + sizeof(uint8_t) + sizeof(struct icmp_header);
-            int payload_len = header->len - (i + sizeof(uint8_t) + sizeof(struct icmp_header));
+            // Printing ICMP payload
+            const u_char* payload = packet+sizeof(eth_header)+sizeof(ip_header)+sizeof(icmp_header);
+            int payload_len = ntohs(ip->total_length) - sizeof(ip_header) - sizeof(icmp_header);
+            
             if (payload_len > 0) 
             {
                 cout << "Payload:" << endl;
@@ -202,59 +260,16 @@ void callback(u_char *user, const struct pcap_pkthdr *header, const u_char *pack
             }
         }
 
-        else if(packet[i]==4) // checking for IP protocol
-        {
-            // extracting the IP protocol from the packet
-            const struct ip_header* ip = (const struct ip_header*)(packet + i + sizeof(uint8_t));
-            // printing the IP header fields
-            cout << "IP header:" << endl;
-            cout << "  Version: " << (int)(ip->version) << endl;
-            cout << "  Header length: " << (int)ip->header_len * 4 << " bytes" << endl;
-            cout << "  Type of service: " << (int)ip->tos << endl;
-            cout << "  Total length: " << ntohs(ip->total_length) <<" bytes" << endl;
-            cout << "  Identification: " << ntohs(ip->id) << endl;
-            cout << "  More fragments: " << (int)ip->more_fragment << endl;
-            cout << "  Don't fragments " << (int)ip->dont_fragment << endl;
-            cout << "  Fragmentation offset: " << (int)((ntohs(ip->frag_offset) & 0x1FFF) * 8) << " bytes" << endl;
-            cout << "  Time to live: " << (int)ip->time_to_live << endl;
-            cout << "  Protocol: " << (int)ip->protocol << endl;
-            cout << "  Checksum: " << ntohs(ip->checksum) << endl;
-            cout << "  Source address: " << inet_ntoa(ip->source) << endl;
-            cout << "  Destination address: " << inet_ntoa(ip->destination) << endl << endl;
+        // printing packet length
+        int packet_length = header->caplen;
+        cout << "Packet Length: " << packet_length << " bytes" << endl;
 
-            // printing the payload
-            const u_char *payload = packet + i + sizeof(uint8_t) + sizeof(ip_header);
-            int payload_len = header->len - (i + sizeof(uint8_t) + sizeof(ip_header));
-            if (payload_len > 0) 
-            {
-                cout << "Payload:" << endl;
-                for (int i = 0; i < payload_len; i++) 
-                {
-                    printf("%02x ", payload[i]);
-                    if ((i + 1) % 16 == 0) 
-                    {
-                        cout << endl;
-                    }
-                }
-                cout << endl << endl << endl;
-            }
-        }
-        // updating the count for the protocol
-        protocols[protocol]++;
+        // printing timestamp
+        cout << "Packet timestamp: " << header->ts.tv_sec << "." << header->ts.tv_usec << endl<<endl<<endl;
+
     }
 
-    // printing the results
-    cout<<"Protocols: "<<endl;
-    for(const auto &p:protocols)
-    {
-        cout<<"  "<<p.ff<<": "<<p.ss<<endl;
-    }
-
-    // display dropped packet count
-    cout << "Dropped packets: " << drop_count << endl<<endl<<endl;
 }
-
-
 
 
 
